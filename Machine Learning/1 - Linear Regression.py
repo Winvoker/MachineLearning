23b4dc1b495cec1b39b9acc52516f2153216dea2
Winvoker <45109898+Winvoker@users.noreply.github.com>
2019-10-28 19:30:35 +0300

    Update 1 - Linear Regression.py

--- Machine Learning/1 - Linear Regression.py
+++ Machine Learning/1 - Linear Regression.py
@@ -7,16 +7,16 @@ Created on Mon Aug 19 14:45:43 2019
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("linear-regression-dataset.csv",sep=";")
-
+# plotting data 
 plt.scatter(df.deneyim,df.maas)
 plt.xlabel("deneyim")
 plt.ylabel("maas")
 plt.show()
 
-from sklearn.linear_model import LinearRegression
-
+# implementing linear regression
 linear_reg=LinearRegression()
 
 x = df.deneyim.values.reshape(-1,1)
@@ -24,8 +24,16 @@ y = df.maas.values.reshape(-1,1)
 
 linear_reg.fit(x,y)
 
-linear_reg.predict([[0]])
+#predicting when deneyim = 10 
+linear_reg.predict([[10]])
 
+# y = b0 + b1.x 
 b0 = linear_reg.intercept_
 
 b1 = linear_reg.coef_
+
+# plotting our fit line
+y_predicted = linear_reg.predict(x)
+
+plt.plot(x,y_predicted,color='r')
+plt.show()
