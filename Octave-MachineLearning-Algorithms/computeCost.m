e12ea84b420b93740b5ccaff5bd1458a1c9d247a
Winvoker <45109898+Winvoker@users.noreply.github.com>
2019-10-28 11:35:38 +0300

    Update computeCost.m

--- Octave-MachineLearning-Algorithms/computeCost.m
+++ Octave-MachineLearning-Algorithms/computeCost.m
@@ -6,19 +6,11 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
 J = 0;
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
+
 
 J = 1/(2*m) * sum(((X*theta)-y).^2);
 
-%predictions = X*theta;              % predictions of hypothesis on examples
-%sqrErrors   = (predictions - y).^2; % squared errors
 
-%J = 1/(2*m) * sum(sqrErrors);
-
-% =========================================================================
 
 end
